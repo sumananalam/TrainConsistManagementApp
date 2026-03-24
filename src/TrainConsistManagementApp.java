c95134b750b7f513b5f9cd564ded4c6edb35adb3
sumananalam <sumananalam@gmail.com>
2026-03-24 09:17:57 +0530

    Base application setup

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,29 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * MAIN CLASS – TrainConsistManagementApp
- */
-/**
- * Use Case 1: Initialize Train and Display Consist Summary
- */
-/**
- * Description:
- * This class represents the entry point of the Train Consist
- * Management Application.
- *
- * At this stage, the application:
- * - Creates an empty train consist
- * - Uses a dynamic List to store bogies
- * - Displays initial bogie count
- * - Prints the current state of the train
- *
- * This use case introduces collection initialization and
- * basic program startup flow.
- *
- * @author Developer
- * @version 1.0
- */
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
